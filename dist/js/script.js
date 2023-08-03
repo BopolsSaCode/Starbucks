4b907ed22cdbc9079ce381d8e2f8cd3c0abd9f52
lovely <cuetolovely0118@gmail.com>
2023-08-03 15:58:05 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,5 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__nav");
+toggleMenu.addEventListener("click", () => {
+    headerNav.classList.toggle("open");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors:{
+          'gurey':'#2c2c32',
+          'dark':'#25252b',
+          'light': '#ffffff',
+          'asul':'#0066b8',
+        },
+  
+      },
+    },
+    plugins: [],
+  }
